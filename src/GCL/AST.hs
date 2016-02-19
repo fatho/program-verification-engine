a043c817ce359202fe286eea0b3995628740cb69
Fabian Thorand <f.thorand@gmail.com>
2016-02-19 13:51:40 +0100

    templates for GCL parts

---
+++ src/GCL/AST.hs
@@ -0,0 +1,3 @@
+{- | Contains the AST of the Guarded Common Language.
+-}
+module GCL.AST where
---
+++ src/GCL/DSL.hs
@@ -0,0 +1,4 @@
+{- | Contains an embedded domain specific language for generating a
+program in the Guarded Common Language represented by "GCL.AST".
+-}
+module GCL.DSL where
