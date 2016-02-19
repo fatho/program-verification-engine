b735541ff001b0914484da27b2074fa86380f4c3
Giovanni Garufi <g.garufi@students.uu.nl>
2016-02-19 15:25:13 +0100

    Added GCL AST

--- src/GCL/AST.hs
+++ src/GCL/AST.hs
@@ -1,3 +1,38 @@
 {- | Contains the AST of the Guarded Common Language.
 -}
 module GCL.AST where
+
+type Name = String
+type Type = String
+type Target = String
+type Variable = (Name, Type)
+data Operator = OpLEQ
+              | OpEQ
+              | OpGEQ
+              | OpLT
+              | OpGT
+              | OpPlus
+              | OpMinus
+              | OpTimes
+              | OpImplies
+              | OpAnd
+              | OpOr
+
+data Program = Program Name Statement
+
+data Statement = Skip
+               | Assert Expression
+               | Assume Expression
+               | Assign (Target, Expression)
+               | Block [Statement]
+               | NDet Statement Statement
+               | While Expression Expression Statement
+               | Var [Variable] Statement
+
+data Expression = IntLit Int
+                | BoolLit Bool
+                | Ref Name
+                | BinaryOp Expression Operator Expression
+                | NegExp Expression
+                | ForAll Variable Expression
+                -- missing name [ expression]
