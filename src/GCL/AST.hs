8740cbecd134395c85e130aa0e14516d6e7dc879
Giovanni Garufi <g.garufi@students.uu.nl>
2016-02-20 23:22:48 +0100

    Added outline of z3 conversion

--- src/GCL/AST.hs
+++ src/GCL/AST.hs
@@ -2,7 +2,7 @@
 {- | Contains the AST of the Guarded Common Language.
 -}
 module GCL.AST where
-    
+
 import Data.String
 
 type Name = String
@@ -10,19 +10,17 @@ type Variable = Name
 
 data Type = BoolType | IntType | ArrayType Type
 
-data RelOp = OpLEQ
+data Operator = OpLEQ
            | OpEQ
            | OpGEQ
            | OpLT
            | OpGT
-
-data IntOp = OpPlus
+           | OpPlus
            | OpMinus
            | OpTimes
-
-data BoolOp = OpImplies
-            | OpAnd
-            | OpOr
+           | OpImplies
+           | OpAnd
+           | OpOr
 
 data Program = Program Name [Decl] [Decl] Statement
 
@@ -40,9 +38,7 @@ data Statement = Skip
 data Expression = IntLit Int
                 | BoolLit Bool
                 | Ref Name
-                | BoolOp BoolOp Expression Expression
-                | IntOp IntOp Expression Expression
-                | RelOp RelOp Expression Expression
+                | ExpOp Operator Expression Expression
                 | Index Expression Expression
                 | RepBy Expression Expression Expression
                 | NegExp Expression
@@ -57,19 +53,19 @@ data Expression (v :: Valueness) (t :: Type) where
   IntLit  :: Int -> Expression 'RValue 'TInt
   BoolLit :: Bool -> Expression 'RValue 'TBool
   Ref     :: Name -> Expression 'LValue t
-  RelOp   :: Expression v1 'TInt 
-          -> RelOp 
+  RelOp   :: Expression v1 'TInt
+          -> RelOp
           -> Expression v2 'TInt
           -> Expression 'RValue 'TBool
   BoolOp  :: Expression v1 'TBool
           -> RelOp
           -> Expression v2 'TBool
           -> Expression 'RValue 'TBool
-  IntOp   :: Expression v1 'TInt 
+  IntOp   :: Expression v1 'TInt
           -> RelOp
           -> Expression v2 'TInt
           -> Expression 'RValue 'TInt
-  Index   :: Expression v1 ('TArray el) 
+  Index   :: Expression v1 ('TArray el)
           -> Expression v2 'TInt
           -> Expression 'RValue el
   RepBy   :: Expression v1 ('TArray el)
@@ -79,12 +75,12 @@ data Expression (v :: Valueness) (t :: Type) where
   BoolNeg :: Expression v 'TBool -> Expression 'RValue 'TBool
   ForAll  :: Variable -> Expression v 'TBool -> Expression 'RValue 'TBool
 -}
-
-instance Num Expression where
-  (+) = IntOp OpPlus
-  (-) = IntOp OpMinus
-  (*) = IntOp OpTimes
-  fromInteger = IntLit . fromInteger
-
-instance IsString Expression where
-  fromString = Ref
+-- 
+-- instance Num Expression where
+--   (+) = IntOp OpPlus
+--   (-) = IntOp OpMinus
+--   (*) = IntOp OpTimes
+--   fromInteger = IntLit . fromInteger
+--
+-- instance IsString Expression where
+--   fromString = Ref
--- src/WLP/Wlp.hs
+++ src/WLP/Wlp.hs
@@ -1,6 +1,7 @@
 module WLP.Wlp where
 
 import GCL.AST
+import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as M
 import Z3.Monad
@@ -10,54 +11,48 @@ data Val = RefVal Ref
          | IntVal Int
          | BoolVal Bool
 
-data Relation = Relation Val Operator Val
-data Predicate = ConstT
-               | ConstF
-               | Single Relation
-               | Impl Predicate Predicate
-               | Conj Predicate Predicate
+--data Relation = Relation Val Operator Val
 
-subst :: Expression -> Target -> Predicate -> Predicate
+type Types = Map Variable Type
+type Env = Map Variable (Z3 AST)
+
+subst :: (Variable, Expression) -> Expression -> Expression
 subst = undefined
-
-toPred :: Expression -> Predicate
-toPred (BinaryOp e1 o e2) = let
-  v1 = getVal e1
-  v2 = getVal e2
-  in Single $ Relation v1 o v2
 
 getVal :: Expression -> Val
 getVal (Ref r) = RefVal r
 getVal (IntLit i) = IntVal i
 getVal (BoolLit b) = BoolVal b
 
-(<^>), (<=>>) :: Predicate -> Predicate -> Predicate
-(<^>) p q = Conj p q
-(<=>>) p q = Impl p q
 
-wlp :: Statement -> Predicate -> Predicate
+wlp :: Statement -> Expression -> Expression
 wlp Skip q = q
-wlp (Assign t e) q = subst e t q
+wlp (Assign e) q = foldr subst q e
 wlp (Block stmts) q = foldr wlp q stmts
-wlp (Assert e) q = toPred e <^> q
-wlp (Assume e) q = toPred e <=>> q
-wlp (NDet s t) q = wlp s q <^> wlp t q
+wlp (Assert e) q = BoolOp OpAnd e q
+wlp (Assume e) q = BoolOp OpImplies e q
+wlp (NDet s t) q = BoolOp OpAnd (wlp s q) (wlp t q)
 
-mkRelation :: Relation -> Z3 AST
-mkRelation = undefined
 
-mkVal :: Val -> Z3 AST
-mkVal = undefined
+mkEnv :: Types -> Env
+mkEnv = M.mapWithKey fromTypes
+  where fromTypes k BoolType = mkFreshBoolVar k
+        fromTypes k IntType = mkFreshIntVar k
 
-toZ3 :: Predicate -> Z3 AST
-toZ3 ConstT = mkTrue
-toZ3 ConstF = mkFalse
-toZ3 (Single r) = mkRelation r
-toZ3 (Impl p q) = do
-  zp <- toZ3 p
-  zq <- toZ3 q
-  mkImplies zp zq
-toZ3 (Conj p q) = do
-  zp <- toZ3 p
-  zq <- toZ3 q
-  mkAnd [zp,zq]
+toZ3 :: Expression -> Env -> Z3 AST
+toZ3 (ExpOp op _e1 _e2) env = do
+  e1 <- mkExp _e1 env
+  e2 <- mkExp _e2 env
+  mkOp op e1 e2
+
+mkExp :: Expression -> Env -> Z3 AST
+mkExp (Ref n) e = fromJust $ M.lookup n e
+mkExp (IntLit n) _ = mkInteger $ toInteger n
+mkExp (BoolLit b) _ = mkBool b
+mkExp exp@(ExpOp o e1 e2) env = toZ3 exp env
+
+mkOp :: Operator -> AST -> AST -> Z3 AST
+mkOp OpPlus v1 v2 = mkAdd [v1, v2]
+mkOp OpMinus v1 v2 = mkSub [v1, v2]
+mkOp OpImplies v1 v2 = mkImplies v1 v2
+mkOp OpAnd v1 v2 = mkAnd [v1, v2]
