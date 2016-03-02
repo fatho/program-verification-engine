538b1a2dac75ca3189a29dd12e51d3b1faf8dae2
Giovanni Garufi <g.garufi@students.uu.nl>
2016-03-02 13:33:38 +0100

    Added d2 example ptoram

--- test/TestPrograms.hs
+++ test/TestPrograms.hs
@@ -7,12 +7,19 @@ import           GCL.DSL
 
 -- * Examples
 
-allPrograms = [ d1, simple, swap, minind ]
+allPrograms = [ d1, d2, simple, swap, minind ]
 
 d1 = program "D1" ["x" `as` int ] ["y" `as` int] $ do
   assume $ 0 < "x"
   while (0 <= "x") (0 < "x") $ do
     "x" $= "x" - 1
+    "y" $= "x"
+  assert $ "y" == 0
+
+d2 = program "D2" ["x" `as` int ] ["y" `as` int] $ do
+  assume $ 2 <= "x"
+  while (0 <= "x") (0 < "x") $ do
+    "x" $= "x" - 2
     "y" $= "x"
   assert $ "y" == 0
 
