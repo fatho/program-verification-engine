94f1efe1c18a24aa970d426faccba604da542450
Fabian Thorand <f.thorand@gmail.com>
2016-03-07 00:46:12 +0100

    included exercise version of minind

--- test/TestPrograms.hs
+++ test/TestPrograms.hs
@@ -7,7 +7,7 @@ import           GCL.DSL
 -- * Examples
 
 allPrograms :: [Either GclError Program]
-allPrograms = [ d1, d2, swap, minind, simple, fixpointCheck ]
+allPrograms = [ d1, d2, swap, minind, minindEx, simple, fixpointCheck ]
 
 d1 :: Either GclError Program
 d1 = program "D1" ["x" `as` int ] ["y" `as` int] $ do
@@ -42,12 +42,36 @@ minind = program "minind" ["a" `as` array int, "i" `as` int, "N" `as` int] ["r" 
    "i0" $= "i"
    var ["min" `as` int] $ do
      ["min", "r"] $$= ["a" ! "i", "i"]
-     "i" $= "i" + 1
+     "i" $= "i" + 1 -- NB: this is different from the version of the exercise, therefore the invariant is simpler
      let iv = "i0" .< "N"  -- capture information about lower bound of range
            /\ "i0" .<= "i"
            /\ "i" .<= "N"  -- we never exceed the range
            /\ "i0" .<= "r"
            /\ "r" .< "i"
+           /\ "min" .== "a" ! "r" -- r points to minimum element found so far
+           /\ forall ("j" `as` int) ("i0" .<= "j" /\ "j" .< "i" ==> "a" ! "r" .<= "a" ! "j")
+     invWhile (Just iv) ("i" .< "N") $ do
+       if_ ("a" ! "i" .< "min")
+         (["min", "r"] $$= ["a" ! "i", "i"])
+         skip
+       "i" $= "i" + 1
+   assert $ forall ("k" `as` int) $ "i0" .<= "k" /\ "k" .< "N" ==> "a" ! "r" .<= "a" ! "k"
+
+
+minindEx :: Either GclError Program
+minindEx = program "minindEx" ["a" `as` array int, "i" `as` int, "N" `as` int] ["r" `as` int] $ do
+  assume $ "i" .< "N"
+  var ["i0" `as` int] $ do
+   "i0" $= "i"
+   var ["min" `as` int] $ do
+     ["min", "r"] $$= ["a" ! "i", "i"]
+     let iv = "i0" .< "N"  -- capture information about lower bound of range
+           /\ "i0" .<= "i"
+           /\ "i" .<= "N"  -- we never exceed the range
+           /\ "i0" .<= "r"
+           -- NB: here we need the more complicated invariant because in the beginning, r == i, whereas every time afterwards we have r < i
+           /\ ("i" .== "i0" ==> "r" .== "i")
+           /\ ("i" .> "i0" ==> "r" .< "i")
            /\ "min" .== "a" ! "r" -- r points to minimum element found so far
            /\ forall ("j" `as` int) ("i0" .<= "j" /\ "j" .< "i" ==> "a" ! "r" .<= "a" ! "j")
      invWhile (Just iv) ("i" .< "N") $ do
